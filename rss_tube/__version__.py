f0e3c2d3af149480efc454e66cb1ccef6c43b26f
dries.k <k.dries@protonmail.com>
2022-01-15 21:13:47 +0100

    v0.5.0

--- rss_tube/__version__.py
+++ rss_tube/__version__.py
@@ -3,4 +3,4 @@ __description__ = 'A simple desktop application for Youtube, based on RSS feeds.
 __url__ = 'https://github.com/seird/rsstube'
 __versionurl__ = 'https://raw.githubusercontent.com/seird/rsstube/master/version.json'
 __author_email__ = 'k.dries@protonmail.com'
-__version__ = "0.4.1"
+__version__ = "0.5.0"
--- version.py
+++ version.py
@@ -6,8 +6,8 @@ VSVersionInfo(
   ffi=FixedFileInfo(
     # filevers and prodvers should be always a tuple with four items: (1, 2, 3, 4)
     # Set not needed items to zero 0.
-    filevers=(0, 4, 1, 0),
-    prodvers=(0, 4, 1, 0),
+    filevers=(0, 5, 0, 0),
+    prodvers=(0, 5, 0, 0),
     # Contains a bitmask that specifies the valid bits 'flags'r
     mask=0x3f,
     # Contains a bitmask that specifies the Boolean attributes of the file.
@@ -33,12 +33,12 @@ VSVersionInfo(
         [StringStruct(u'Comments', u'RSS Tube - Youtube RSS Reader Client'),
 	StringStruct(u'CompanyName', u''),
         StringStruct(u'FileDescription', u'Youtube RSS Reader Client'),
-        StringStruct(u'FileVersion', u'0.4.1'),
+        StringStruct(u'FileVersion', u'0.5.0'),
         StringStruct(u'InternalName', u'rss-tube'),
         StringStruct(u'LegalCopyright', u''),
         StringStruct(u'OriginalFilename', u'rsstube.exe'),
         StringStruct(u'ProductName', u'RSS Tube'),
-        StringStruct(u'ProductVersion', u'0.4.1')])
+        StringStruct(u'ProductVersion', u'0.5.0')])
       ]), 
     VarFileInfo([VarStruct(u'Translation', [0, 1200])])
   ]
