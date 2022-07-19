5b8d23ce3cfefd2ff87f3c10b408c8d18708e204
dries.k <k.dries@protonmail.com>
2022-07-19 19:55:11 +0200

    add portable build

--- build-win.ps1
+++ build-win.ps1
@@ -3,7 +3,12 @@ echo "Creating executable"
 try {C:/Python39/Scripts/pyinstaller rss-tube.spec}
 catch {pyinstaller rss-tube.spec}
 
-try {Remove-Item "dist/rsstube/opengl32sw.dll"} catch {}
+$compress = @{
+  Path = "dist\rsstube"
+  CompressionLevel = "Optimal"
+  DestinationPath = "rsstube-portable-win.zip"
+}
+Compress-Archive @compress
 
 echo "Creating installer"
 iscc rss-tube.iss
