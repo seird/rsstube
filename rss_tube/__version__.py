40405cfbf3bd0b644bffd902aea3587553273275
dries.k <k.dries@protonmail.com>
2023-04-08 14:19:54 +0200

    v0.6.15

--- rss_tube/__version__.py
+++ rss_tube/__version__.py
@@ -3,4 +3,4 @@ __description__ = 'A simple desktop application for Youtube, based on RSS feeds.
 __url__ = 'https://github.com/seird/rsstube'
 __versionurl__ = 'https://raw.githubusercontent.com/seird/rsstube/develop/version.json'
 __author_email__ = 'k.dries@protonmail.com'
-__version__ = "0.6.14"
+__version__ = "0.6.15"
